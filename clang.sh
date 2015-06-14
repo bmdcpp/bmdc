e92d045dffd0b9c1a62b5c14484e109015f7c2e7
Mank <freedcpp@seznam.cz>
2015-06-14 18:55:18 +0200

    cleanup of clang.sh

--- clang.sh
+++ clang.sh
@@ -5,17 +5,6 @@ export CC="clang";
 echo $CC;
 export CXX="clang++";
 echo $CXX;
-#export CCFLAGS="${CCFLAGS} -std=c++11 -pthread";
-#echo $CCFLAGS;
-#export CPPFLAGS="${CPPFLAGS} -pthread";
-#echo $CPPFLAGS;
-#export CXXFLAGS="${CPPFLAGS}";
-#echo $CXXFLAGS;
-#-stdlib=libstdc++
-#export CFLAGS="${CFLAGS}  -std=c++11 -pthread";
-#echo $CFLAGS;
-#export LINKFLAGS="${LINKFLAGS} -pthread";
-#echo $LINKFLAGS;
 mkdir -p $HOME/clang-bmdc/
 scons --debug=includes PREFIX=$HOME/clang-bmdc/ debug=1 
 scons install
