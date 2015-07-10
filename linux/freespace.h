591ff7f7b22e0136a165b18df0f674fb202ef19f
Mank <freedcpp@seznam.cz>
2015-07-10 12:42:23 +0200

    Add shm fsname to forbiden name in the print_mount

--- linux/freespace.h
+++ linux/freespace.h
@@ -61,7 +61,9 @@ static void print_mount(std::string &s,const struct mntent *fs)
 			return;		
 		if(strcmp (fs->mnt_fsname,"udev")	== 0)
 			return;	
-		if(strcmp (fs->mnt_fsname,"cgroup_root")	== 0)
+		if(strcmp (fs->mnt_fsname,"shm")	== 0)
+			return;
+		if(strcmp (fs->mnt_fsname,"cgroup_root") == 0)
 			return;			
 			
 		if(std::string(fs->mnt_fsname).find("fuse") != std::string::npos)	
