66f395d0665491532e2eea7f05cb3a10ad7c6abb
Mank16 <freedcpp@seznam.cz>
2017-04-25 18:48:15 +0200

    Potentialy fix openssl 1.1.0a+

--- dcpp/CryptoManager.h
+++ dcpp/CryptoManager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2017 Jacek Sieka, arnetheduck on gmail point com
+ * Copyright (C) 2001-2012 Jacek Sieka, arnetheduck on gmail point com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,110 +13,68 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef DCPLUSPLUS_DCPP_CRYPTO_MANAGER_H
-#define DCPLUSPLUS_DCPP_CRYPTO_MANAGER_H
+#pragma once
 
-#include "typedefs.h"
+#ifdef _WIN32
+#include <wincrypt.h>
+#endif
 
 #include "SettingsManager.h"
-
 #include "Exception.h"
 #include "Singleton.h"
-
-#include "SSL.h"
+#include "SSLSocket.h"
 
 namespace dcpp {
-
-using std::pair;
-using std::string;
 
 STANDARD_EXCEPTION(CryptoException);
 
 class CryptoManager : public Singleton<CryptoManager>
 {
 public:
-	typedef pair<bool, string> SSLVerifyData;
+    string makeKey(const string& aLock);
+    const string& getLock() { return lock; }
+    const string& getPk() { return pk; }
+    bool isExtended(const string& aLock) { return strncmp(aLock.c_str(), "EXTENDEDPROTOCOL", 16) == 0; }
 
-	enum TLSTmpKeys {
-		KEY_FIRST = 0,
-		KEY_DH_512 = KEY_FIRST,
-		KEY_DH_1024,
-		KEY_DH_2048,
-		KEY_DH_4096,
-		KEY_RSA_512,
-		KEY_RSA_1024,
-		KEY_RSA_2048,
-		KEY_LAST
-	};
+    void decodeBZ2(const uint8_t* is, size_t sz, string& os);
 
-	enum SSLContext {
-		SSL_CLIENT,
-		SSL_SERVER
-	};
+    SSLSocket* getClientSocket(bool allowUntrusted);
+    SSLSocket* getServerSocket(bool allowUntrusted);
 
-	string makeKey(const string& aLock);
-	const string& getLock() { return lock; }
-	const string& getPk() { return pk; }
-	bool isExtended(const string& aLock) { return strncmp(aLock.c_str(), "EXTENDEDPROTOCOL", 16) == 0; }
+    void loadCertificates() noexcept;
+    void generateCertificate();
+    bool checkCertificate() noexcept;
+    const vector<uint8_t>& getKeyprint() const noexcept;
 
-	void decodeBZ2(const uint8_t* is, size_t sz, string& os);
-
-	SSL_CTX* getSSLContext(SSLContext wanted);
-
-	void loadCertificates() noexcept;
-	void generateCertificate();
-	bool checkCertificate() noexcept;
-	const ByteVector& getKeyprint() const noexcept;
-
-	bool TLSOk() const noexcept;
-
-	static void locking_function(int mode, int n, const char* /*file*/, int /*line*/);
-	static DH* tmp_dh_cb(SSL* /*ssl*/, int /*is_export*/, int keylength);
-	static RSA* tmp_rsa_cb(SSL* /*ssl*/, int /*is_export*/, int keylength);
-	static int verify_callback(int preverify_ok, X509_STORE_CTX *ctx);
-
-	static int idxVerifyData;
-
+    bool TLSOk() const noexcept;
 private:
 
-	friend class Singleton<CryptoManager>;
+    friend class Singleton<CryptoManager>;
 
-	CryptoManager();
-	virtual ~CryptoManager();
+    CryptoManager();
+    virtual ~CryptoManager();
 
-	ssl::SSL_CTX clientContext;
-	ssl::SSL_CTX serverContext;
+    ssl::SSL_CTX clientContext;
+    ssl::SSL_CTX clientVerContext;
+    ssl::SSL_CTX serverContext;
+    ssl::SSL_CTX serverVerContext;
 
-	void sslRandCheck();
+    ssl::DH dh;
 
-	int getKeyLength(TLSTmpKeys key);
-	DH* getTmpDH(int keyLen);
-	RSA* getTmpRSA(int keyLen);
+    bool certsLoaded;
 
-	static void* tmpKeysMap[KEY_LAST];
-	static CriticalSection* cs;
-	static char idxVerifyDataName[];
-	static SSLVerifyData trustedKeyprint;
+    vector<uint8_t> keyprint;
+    const string lock;
+    const string pk;
 
-	ByteVector keyprint;
-	const string lock;
-	const string pk;
-	bool certsLoaded;
-	
-	string keySubst(const uint8_t* aKey, size_t len, size_t n);
-	
-	bool isExtra(const uint8_t b) {
-		return (b == 0 || b==5 || b==124 || b==96 || b==126 || b==36);
-	}
-
-	static string getNameEntryByNID(X509_NAME* name, int nid) noexcept;
-
-	void loadKeyprint(const string& file) noexcept;
+    string keySubst(const uint8_t* aKey, size_t len, size_t n);
+    bool isExtra(uint8_t b) {
+        return (b == 0 || b==5 || b==124 || b==96 || b==126 || b==36);
+    }
+    void loadKeyprint(const string& file) noexcept;
 };
 
 } // namespace dcpp
-
-#endif // !defined(CRYPTO_MANAGER_H)
--- dcpp/SSLSocket.h
+++ dcpp/SSLSocket.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2017 Jacek Sieka, arnetheduck on gmail point com
+ * Copyright (C) 2001-2012 Jacek Sieka, arnetheduck on gmail point com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,76 +13,67 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef DCPLUSPLUS_DCPP_SSLSOCKET_H
-#define DCPLUSPLUS_DCPP_SSLSOCKET_H
+#pragma once
 
-#include "typedefs.h"
-
-#include "CryptoManager.h"
 #include "Socket.h"
 #include "Singleton.h"
-
 #include "SSL.h"
+
+#ifndef SSL_SUCCESS
+#define SSL_SUCCESS 1
+#endif
 
 namespace dcpp {
 
-using std::unique_ptr;
-using std::string;
-
-class SSLSocketException : public SocketException 
-{
+class SSLSocketException : public SocketException {
 public:
 #ifdef _DEBUG
-	SSLSocketException(const string& aError) noexcept : SocketException("SSLSocketException: " + aError) { }
+    SSLSocketException(const string& aError) noexcept : SocketException("SSLSocketException: " + aError) { }
 #else //_DEBUG
-	SSLSocketException(const string& aError) noexcept : SocketException(aError) { }
+    SSLSocketException(const string& aError) noexcept : SocketException(aError) { }
 #endif // _DEBUG
-	SSLSocketException(int aError) noexcept : SocketException(aError) { }
 
-	virtual ~SSLSocketException() throw() { }
+    virtual ~SSLSocketException() throw() { }
 };
 
-class SSLSocket : public Socket 
-{
+class CryptoManager;
+
+class SSLSocket : public Socket {
 public:
-	using Socket::connect;
-	SSLSocket(CryptoManager::SSLContext context, bool allowUntrusted, const string& expKP);
-	/** Creates an SSL socket without any verification */
-	SSLSocket(CryptoManager::SSLContext context);
+    virtual ~SSLSocket() { }
 
-	virtual ~SSLSocket() { verifyData.reset(); }
+    virtual void accept(const Socket& listeningSocket);
+    virtual void connect(const string& aIp, uint16_t aPort);
+    virtual int read(void* aBuffer, int aBufLen);
+    virtual int write(const void* aBuffer, int aLen);
+    virtual std::pair<bool, bool>  wait(uint32_t millis, bool checkRead, bool checkWrite);
+    virtual void shutdown() noexcept;
+    virtual void close() noexcept;
 
-	virtual void accept(const Socket& listeningSocket);
-	virtual void connect(const string& aIp, const uint16_t& aPort);
-	virtual int read(void* aBuffer, int aBufLen);
-	virtual int write(const void* aBuffer, int aLen);
-	virtual std::pair<bool, bool> wait(int32_t millis, bool checkRead, bool checkWrite);
-	virtual void shutdown() noexcept;
-	virtual void close() noexcept;
+    virtual bool isSecure() const noexcept { return true; }
+    virtual bool isTrusted() const noexcept;
+    virtual std::string getCipherName() const noexcept;
+    virtual vector<uint8_t> getKeyprint() const noexcept;
 
-	virtual bool isSecure() const noexcept { return true; }
-	virtual bool isTrusted() const noexcept;
-	virtual string getCipherName() const noexcept;
-	virtual ByteVector getKeyprint() const noexcept;
-	virtual bool verifyKeyprint(const string& expKeyp, bool allowUntrusted) noexcept;
+    virtual bool waitConnected(uint32_t millis);
+    virtual bool waitAccepted(uint32_t millis);
 
-	virtual bool waitConnected(int32_t millis);
-	virtual bool waitAccepted(int32_t millis);
 
 private:
+    friend class CryptoManager;
 
-	SSL_CTX* ctx;
-	ssl::SSL ssl;
+    SSLSocket(SSL_CTX* context);
+    SSLSocket(const SSLSocket&);
+    SSLSocket& operator=(const SSLSocket&);
 
-	unique_ptr<CryptoManager::SSLVerifyData> verifyData;	// application data used by CryptoManager::verify_callback(...)
+    SSL_CTX* ctx;
+    ssl::SSL ssl;
 
-	int checkSSL(int ret);
-	bool waitWant(int ret, int32_t millis);
+    int checkSSL(int ret);
+    bool waitWant(int ret, uint32_t millis);
 };
 
 } // namespace dcpp
-
-#endif // SSLSOCKET_H
