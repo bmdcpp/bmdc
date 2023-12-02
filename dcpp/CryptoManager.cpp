7386eca2dc3540dda84620448a75f48a93327a56
Marek PIstorius <me@maniu.eu>
2023-12-02 14:30:32 +0100

    Update CryptoManager.cpp
    ignore deprecated warns from ssl

--- dcpp/CryptoManager.cpp
+++ dcpp/CryptoManager.cpp
@@ -16,6 +16,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 #include "stdinc.h"
 #include "CryptoManager.h"
 
@@ -783,3 +786,4 @@ string CryptoManager::makeKey(const string& aLock) {
 }
 
 } // namespace dcpp
+#pragma GCC diagnostic pop
