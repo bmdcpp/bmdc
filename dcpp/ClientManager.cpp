e9026583cbcb0cc32e8d715fae91cb4409fdd01f
Marek Pistorius <me@maniu.eu>
2026-06-21 12:15:33 +0200

    Add support for NMDCS hub URL handling
    Signed-off-by: Marek Pistorius <me@maniu.eu>

--- dcpp/ClientManager.cpp
+++ dcpp/ClientManager.cpp
@@ -52,6 +52,8 @@ Client* ClientManager::getClient(const string& aHubURL) {
 		pClient = new AdcHub(aHubURL, false);
 	} else if(Util::strnicmp("adcs://", aHubURL.c_str(), 7) == 0) {
 		pClient = new AdcHub(aHubURL, true);
+	} else if(Util::strnicmp("nmdcs://", aHubURL.c_str(), 8) == 0) {
+		c = new NmdcHub(aHubURL, true);	
 	} else {
 		pClient = new NmdcHub(aHubURL);
 	}
